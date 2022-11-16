4782f1c8d322177a6a16452f918f438b42843ea3
hyb2419805602 <2419805602@qq.com>
2022-11-16 22:39:21 +0800

    333

--- mybatis-demo/src/main/java/com/itheima/MyBatisDemo.java
+++ mybatis-demo/src/main/java/com/itheima/MyBatisDemo.java
@@ -1,16 +1,30 @@
 package com.itheima;
 
+import com.itheima.pojo.User;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.InputStream;
+import java.util.List;
 
 public class MyBatisDemo {
     public static void main(String[] args) throws Exception {
         // 1. 加载mybatis核心配置文件, 获取sqlSessionFactory.
-        String resource = "org/mybatis/example/mybatis-config.xml";
+        String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+
+        // 2. 获取sqlSession对象, 用它来执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 3. 执行sql
+        List<User> users = sqlSession.selectList("test.selectAll");
+        System.out.println(users);
+
+        // 4. 释放资源
+        sqlSession.close();
     }
 }
