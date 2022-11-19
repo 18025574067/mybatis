26f6f87894cfecb621cde91ca804fd03887d3d1a
hyb2419805602 <2419805602@qq.com>
2022-11-19 22:52:16 +0800

    1111

---
+++ mybatis-demo/src/main/java/com/itheima/pojo/Brand.java
@@ -0,0 +1,72 @@
+package com.itheima.pojo;
+
+public class Brand {
+
+    // 自定义格式化代码: File-settings-Keymap-reformat-(+ Ctrl-Shift-Tab);
+    private Integer id;
+    private String brand_name;
+    private String company_name;
+    private Integer ordered;
+    private String description;
+    private Integer status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBrand_name() {
+        return brand_name;
+    }
+
+    public void setBrand_name(String brand_name) {
+        this.brand_name = brand_name;
+    }
+
+    public String getCompany_name() {
+        return company_name;
+    }
+
+    public void setCompany_name(String company_name) {
+        this.company_name = company_name;
+    }
+
+    public Integer getOrdered() {
+        return ordered;
+    }
+
+    public void setOrdered(Integer ordered) {
+        this.ordered = ordered;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Brand{" +
+                "id=" + id +
+                ", brand_name='" + brand_name + '\'' +
+                ", company_name='" + company_name + '\'' +
+                ", ordered=" + ordered +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                '}';
+    }
+}
