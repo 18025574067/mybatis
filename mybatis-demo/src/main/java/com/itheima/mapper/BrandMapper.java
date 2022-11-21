be2f3f4f277cc6cbd29a8a4193990a9f6db40f76
hyb2419805602 <2419805602@qq.com>
2022-11-21 22:33:46 +0800

    1111

--- mybatis-demo/src/main/java/com/itheima/mapper/BrandMapper.java
+++ mybatis-demo/src/main/java/com/itheima/mapper/BrandMapper.java
@@ -2,8 +2,10 @@ package com.itheima.mapper;
 
 import com.itheima.pojo.Brand;
 import com.itheima.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
 
@@ -12,4 +14,27 @@ public interface BrandMapper {
      */
     List<Brand> selectAll();
 
+    /**
+     * 查看详情
+     */
+    Brand selectById(int id);
+
+    /**
+     * 条件查询
+     * * 参数接收
+     *      1. 散装参数: 如果方法中有多个参数, 需要使用@Parm("SQL参数占位符")
+     *      2. 对象参数
+     *      3. map集合参数
+     *
+     *
+     * @param status
+     * @param companyName
+     * @param brandName
+     * @return
+     */
+    List<Brand> selectByCondition(@Param("status") int status, @Param("companyName") String companyName, @Param("brandName") String brandName);
+
+    /** List<Brand> selectByCondition(Brand brand);
+
+    List<Brand> selectByCondition(Map map);*/
 }
--- mybatis-demo/src/test/java/com/itheima/test/MyBatisTest.java
+++ mybatis-demo/src/test/java/com/itheima/test/MyBatisTest.java
@@ -34,4 +34,57 @@ public class MyBatisTest {
         // 5. 释放资源
         sqlSession.close();
     }
+
+    @Test
+    public void testSelectById() throws IOException {
+        // 接收参数
+        int id = 1;
+        // 1. 获取sqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        // 2. 获取sqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 3. 获取Mapper接口的代理对象
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        // 4. 执行方法
+        Brand brand = brandMapper.selectById(id);
+        System.out.println(brand);
+
+        // 5. 释放资源
+        sqlSession.close();
+    }
+
+    @Test
+    public void testSelectByCondition() throws IOException {
+        // 接收参数
+        int status = 1;
+        String companyName = "华为";
+        String brandName = "华为";
+
+        // 处理参数
+        companyName = "%" + companyName + "%";
+        brandName = "%" + brandName + "%";
+
+        // 1. 获取sqlSessionFactory
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        // 2. 获取sqlSession对象
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // 3. 获取Mapper接口的代理对象
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        // 4. 执行方法
+        List<Brand> brands = brandMapper.selectByCondition(status, companyName, brandName);
+        System.out.println(brands);
+
+        // 5. 释放资源
+        sqlSession.close();
+    }
 }
