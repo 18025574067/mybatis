3d062b0917db6402b5e8e661c63e761751ea5199
hyb2419805602 <2419805602@qq.com>
2022-11-21 23:13:39 +0800

    1111

--- mybatis-demo/src/main/java/com/itheima/mapper/BrandMapper.java
+++ mybatis-demo/src/main/java/com/itheima/mapper/BrandMapper.java
@@ -33,8 +33,6 @@ public interface BrandMapper {
      * @return
      */
     // List<Brand> selectByCondition(@Param("status") int status, @Param("companyName") String companyName, @Param("brandName") String brandName);
-
     // List<Brand> selectByCondition(Brand brand);
-
     List<Brand> selectByCondition(Map map);
 }
